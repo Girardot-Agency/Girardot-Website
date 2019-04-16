a00f2111bb97671524f63305820345e13059c3e3
Luke Boga Mitchell <lbogamitchell@gmail.com>
2019-04-16 23:44:49 +0100

    Lightbox - almost done!

--- components/copy.js
+++ components/copy.js
@@ -1,10 +1,10 @@
 import Markdown from "markdown-to-jsx";
-// import Lightbox from "react-image-lightbox";
-// import "react-image-lightbox/style.css";
 import react, {Component} from "react";
-import Modal from "react-modal";
 import ReactPlayer from "react-player";
 import styled, {css} from "styled-components";
+
+import Lightbox from "react-image-lightbox";
+import "react-image-lightbox/style.css";
 
 import {
   $_BaseUnit
@@ -54,66 +54,114 @@ function Player (props = {}) {
  * =Gallery
 ************************************************************/
 
-Modal.setAppElement("#__next");
+// Modal.setAppElement("#__next");
 
-const customStyles = {
-  overlay: {
-    backgroundColor: "rgba(0, 0, 0, .5)"
-  },
-  content : {
-    top: "50%",
-    left: "50%",
-    right: "auto",
-    bottom: "auto",
-    marginRight: "-50%",
-    transform: "translate(-50%, -50%)",
-    maxHeight: "95%",
-    maxWidth: "95%",
-    zIndex: "9999"
-  }
-};
+// const customStyles = {
+//   overlay: {
+//     backgroundColor: "rgba(0, 0, 0, .5)"
+//   },
+//   content : {
+//     top: "50%",
+//     left: "50%",
+//     right: "auto",
+//     bottom: "auto",
+//     marginRight: "-50%",
+//     transform: "translate(-50%, -50%)",
+//     maxHeight: "95%",
+//     maxWidth: "95%",
+//     zIndex: "9999"
+//   }
+// };
 
-class ImageModal extends Component {
+// class ImageModal extends Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       modalIsOpen: false
+//     };
+//   }
+
+//   handleOpenModal() {
+//     this.setState({
+//       modalIsOpen: true
+//     });
+//   }
+
+//   handleCloseModal() {
+//     this.setState({
+//       modalIsOpen: false
+//     });
+//   }
+
+//   render() {
+//     return (<>
+//       <img
+//         onClick={this.handleOpenModal.bind(this)}
+//         src={this.props.image}
+//       />
+
+//       <Modal
+//         isOpen={this.state.modalIsOpen}
+//         closeTimeoutMS={400}
+//         style={customStyles}
+//       >
+//         <img
+//           src={this.props.image}
+//         />
+//       </Modal>
+//     </>)
+//   }
+// }
+
+
+class Gallery extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      modalIsOpen: false
+      photoIndex: 0,
+      isOpen: false,
     };
   }
 
-  handleOpenModal() {
-    this.setState({
-      modalIsOpen: true
-    });
-  }
-
-  handleCloseModal() {
-    this.setState({
-      modalIsOpen: false
-    });
+  handleClick() {
+    this.setState({ isOpen: true })
   }
 
   render() {
-    return (<>
-      <img
-        onClick={this.handleOpenModal.bind(this)}
-        src={this.props.image}
-      />
+    const { photoIndex, isOpen } = this.state;
+    console.log(isOpen)
 
-      <Modal
-        isOpen={this.state.modalIsOpen}
-        closeTimeoutMS={400}
-        style={customStyles}
-      >
-        <img
-          src={this.props.image}
-        />
-      </Modal>
-    </>)
+    return (
+
+      <div>
+        <button type="button" onClick={this.handleClick.bind(this)}>
+          Open Lightbox
+        </button>
+
+        {isOpen && (
+          <Lightbox
+            mainSrc={this.props.images[photoIndex]}
+            nextSrc={this.props.images[(photoIndex + 1) % this.props.images.length]}
+            prevSrc={this.props.images[(photoIndex + this.props.images.length - 1) % this.props.images.length]}
+            onCloseRequest={() => this.setState({ isOpen: false })}
+            onMovePrevRequest={() =>
+              this.setState({
+                photoIndex: (photoIndex + this.props.images.length - 1) % this.props.images.length,
+              })
+            }
+            onMoveNextRequest={() =>
+              this.setState({
+                photoIndex: (photoIndex + 1) % this.props.images.length,
+              })
+            }
+          />
+        )}
+      </div>
+    );
   }
 }
-
 
 /**
  * =Copy
@@ -166,12 +214,13 @@ function Copy ({title, copy, gallery}) {
 
       { Array.isArray(gallery) && gallery.length
         ? (
-          gallery.map((image, index) => {
+          // gallery.map((image, index) => {
 
-            return (
-              <ImageModal image={image} />
-            )
-          })
+          //   return (
+          //     <ImageModal image={image} />
+          //   )
+          // })
+          <Gallery images={gallery} />
         )
         : ""
       }
