eeba4e9d423fa4b6dc8a0d39b27955bae73a7967
Luke Boga Mitchell <lbogamitchell@gmail.com>
2019-04-16 20:28:38 +0100

    Jobs page - WIP

--- pages/jobs-page.js
+++ pages/jobs-page.js
@@ -1,6 +1,71 @@
-import Link from "next/link"
+import Markdown from "markdown-to-jsx";
+import React, {Component} from "react";
+import {withRouter} from "next/router"
+import styled, {css} from "styled-components";
 
-export default props => (<>
-  <h1>Hello</h1>
-  <Link href="/about"><a>There</a></Link>
-</>)
+import ContentWrapper from "../components/content-wrapper";
+import DefaultLayout from "../layouts/default";
+
+import {
+  $_Flex,
+  $_BaseUnit,
+  $_Screen
+} from "../assets/styles/mixins.css";
+
+import {
+  COL,
+  TYPE
+} from "../assets/styles/theme.css.js"
+
+const JobsInner_SC = styled.div`
+  line-height: 1.5;
+  margin: ${$_BaseUnit(10)} auto;
+  width: ${$_BaseUnit(50)};
+    max-width: 100%;
+
+  h1, h2, h3, h4, h5, h6, p, ul, ol {
+    margin-bottom: ${TYPE.scale.sm};
+
+    &:nth-child(1n +2 ) {
+      margin-top: 1em;
+    }
+  }
+
+  h2 {
+    color: ${COL.brand_main_base};
+    font-size: ${TYPE.scale.md};
+      font-weight: normal;
+  }
+
+  ul {
+    list-style: square;
+    padding-left: ${$_BaseUnit(2.5)};
+  }
+`;
+
+class JobPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pageData: props.router.query
+    }
+  }
+
+  render () {
+    const {router} = this.props;
+    const pageData = this.state.pageData;
+    console.log(pageData);
+
+    return (
+      <DefaultLayout>
+        <ContentWrapper>
+          <JobsInner_SC>
+            <Markdown children={pageData.body} />
+          </JobsInner_SC>
+        </ContentWrapper>
+      </DefaultLayout>
+    )
+  }
+}
+
+export default withRouter(JobPage);
