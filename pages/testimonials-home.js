3ed0515560c7241865ac0790a5e841e53d6f7986
Luke Boga Mitchell <lbogamitchell@gmail.com>
2019-04-16 18:25:52 +0100

    Testimonials - WIP

--- pages/testimonials-home.js
+++ pages/testimonials-home.js
@@ -1,6 +1,70 @@
 import Link from "next/link"
+import {withRouter} from "next/router"
+import React, {Component} from "react";
+import styled from "styled-components";
 
-export default props => (<>
-  <h1>Hello</h1>
-  <Link href="/about"><a>There</a></Link>
-</>)
+import ContentWrapper from "../components/content-wrapper";
+import DefaultLayout from "../layouts/default";
+import Grid from "../components/grid";
+import Profile, {profiles} from "../components/profile";
+import SectionTitle from "../components/section-title";
+
+import exportMap from "../static/db/export-map.json";
+import {
+  $_Flex,
+  $_BaseUnit
+} from "../assets/styles/mixins.css";
+import {
+  COL,
+  TYPE
+} from "../assets/styles/theme.css.js"
+
+/**
+ * =Our people
+************************************************************/
+
+const Header_SC = styled.div`
+  margin-top: ${$_BaseUnit(5)};
+`;
+
+class TestimonialsHome extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pageData: props.router.query
+    }
+  }
+
+  handleClick() {
+    this.setState({
+      active: !this.state.active,
+      activeClass: this.state.active
+        ? "is-inactive"
+        : "is-active"
+    });
+  }
+
+  render() {
+    const {router} = this.props;
+    const pageData = this.state.pageData;
+
+    return (
+      <DefaultLayout>
+        <Header_SC>
+          <SectionTitle title={pageData.title} />
+        </Header_SC>
+
+        <ContentWrapper>
+
+          {/* { pageData.ourPeopleRels &&
+            <Grid
+              cells={profiles(pageData.ourPeopleRels)}
+            />
+          } */}
+        </ContentWrapper>
+      </DefaultLayout>
+    );
+  }
+}
+
+export default withRouter(TestimonialsHome);
