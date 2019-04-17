6d4e3cca04a215fc517a4825c402d8109170afb3
Luke Boga Mitchell <lbogamitchell@gmail.com>
2019-04-17 18:54:12 +0100

    Finish theme refac v4

--- assets/styles/theme/_style.js
+++ assets/styles/theme/_style.js
@@ -1,7 +1,7 @@
 import BASE from "./base.style";
 import COL from "./colors.style";
 import SCREEN from "./mqs.style";
-import TYPE from "./TYPE.style";
+import TYPE from "./type.style";
 
 export {
   BASE,
