378ba8cf6883473b863b980b9b91868a17aa77f5
Luke Boga Mitchell <lbogamitchell@gmail.com>
2019-04-25 14:33:07 +0100

    CMS branches

--- next.config.js
+++ next.config.js
@@ -1,30 +1,35 @@
 const fs = require("fs-extra");
 const path = require("path");
 
-const {ENV, DIR, ASSET} = require("./lib/_config");
+const { ENV, BRANCH, DIR, ASSET } = require("./lib/_config");
+
 const exportMap = require(ASSET.exportMap);
 
 module.exports = {
-  webpack: (config, {defaultLoaders}) => {
-    config.module.rules.push({
-      test: /\.css$/,
-      use: [
-        defaultLoaders.babel,
-        {
-          loader: require("styled-jsx/webpack").loader,
-          options: {type: "scoped"}
-        }
-      ]
-    });
-    return config;
-  },
-  exportPathMap: (defaultPathMap, buildInfo = {}) => {
-    const {dev, dir, outDir, distDir, buildId} = buildInfo;
+	webpack: (config, { defaultLoaders }) => {
+		config.module.rules.push({
+			test: /\.css$/,
+			use: [
+				defaultLoaders.babel,
+				{
+					loader: require("styled-jsx/webpack").loader,
+					options: {
+						type: "scoped"
+					}
+				}
+			]
+		});
+		return config;
+	},
+	exportPathMap: (defaultPathMap, buildInfo = {}) => {
+		const { dev, dir, outDir, distDir, buildId } = buildInfo;
 
-    if (!dev) {
-      fs.copy(DIR.exports, path.resolve(DIR.out));
-      return exportMap;
-    }
-    return defaultPathMap;
-  }
-}
+		if (!dev) {
+			const cmsDir = `${DIR.exports}/cms-${BRANCH}`;
+			fs.copy(cmsDir, path.resolve(`${DIR.out}/admin`));
+
+			return exportMap;
+		}
+		return defaultPathMap;
+	}
+};
