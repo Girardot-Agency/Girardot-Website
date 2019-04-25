e54f729de889fda4818216752e27c30be5da600f
Luke Boga Mitchell <lbogamitchell@gmail.com>
2019-04-25 13:22:51 +0100

    Prettier

--- components/card/index.js
+++ components/card/index.js
@@ -2,21 +2,26 @@ import Link from "next/link";
 import styled from "styled-components";
 
 // Helpers/mixins/theme
-import {transformImage} from "../../lib/helpers";
-import {_baseUnit, _hidden, _flex, _transition} from "../../assets/styles/mixins/_style";
-import {COL, SCREEN, TYPE} from "../../assets/styles/theme/_style";
+import { transformImage } from "../../lib/helpers";
+import {
+	_baseUnit,
+	_hidden,
+	_flex,
+	_transition
+} from "../../assets/styles/mixins/_style";
+import { COL, SCREEN, TYPE } from "../../assets/styles/theme/_style";
 
 import exportMap from "../../static/db/export-map.json";
 
 /**
  * =Card
-************************************************************/
+ ************************************************************/
 
 /**
  * =Card:styles
-******************************/
+ ******************************/
 
-const Card_Styled = styled.section`
+const Card_Styled = styled.section `
   overflow: hidden;
   position: relative;
 
@@ -69,100 +74,80 @@ const Card_Styled = styled.section`
 
 /**
  * =Card:component
-******************************/
+ ******************************/
 
-function Card ({pageData}) {
-  const route = {
-    pathname: pageData.page,
-    query: pageData.query
-  };
+function Card({ pageData }) {
+	const route = {
+		pathname: pageData.page,
+		query: pageData.query
+	};
 
-  return (
-    <Card_Styled>
+	return (
+		<Card_Styled>
+			<div className="Card-image">
+				<picture>
+					<source
+						media={`${SCREEN.lg}`}
+						sizes={"33.33vw"}
+						srcSet={`
+							${transformImage(pageData.query.cardImage, { w: 300 })} 300w,
+							${transformImage(pageData.query.cardImage, { w: 500 })} 500w
+						`}
+					/>
+					<source
+						media={`${SCREEN.sm}`}
+						sizes={"50vw"}
+						srcSet={`
+							${transformImage(pageData.query.cardImage, { w: 300 })} 300w,
+							${transformImage(pageData.query.cardImage, { w: 500 })} 500w
+			            `}
+					/>
+					<source
+						sizes={"100vw"}
+						srcSet={`
+							${transformImage(pageData.query.cardImage, { w: 400 })} 400w,
+							${transformImage(pageData.query.cardImage, { w: 600 })} 600w
+			            `}
+					/>
+					<img
+						src={transformImage(pageData.query.cardImage)}
+						alt={pageData.query.title}
+					/>
+				</picture>
+			</div>
 
-      <div className="Card-image">
-        <picture>
-          <source
-            media={`${SCREEN.lg}`}
-            sizes={"33.33vw"}
-            srcSet={`
-              ${transformImage(pageData.query.cardImage, {w: 300})} 300w,
-              ${transformImage(pageData.query.cardImage, {w: 500})} 500w
-            `}
-          />
-          <source
-            media={`${SCREEN.sm}`}
-            sizes={"50vw"}
-            srcSet={`
-              ${transformImage(pageData.query.cardImage, {w: 300})} 300w,
-              ${transformImage(pageData.query.cardImage, {w: 500})} 500w
-            `}
-          />
-          <source
-            sizes={"100vw"}
-            srcSet={`
-              ${transformImage(pageData.query.cardImage, {w: 400})} 400w,
-              ${transformImage(pageData.query.cardImage, {w: 600})} 600w
-            `}
-          />
-          <img
-            src={transformImage(pageData.query.cardImage)}
-            alt={pageData.query.title}
-          />
-        </picture>
-      </div>
+			<h3 className="Card-title">{pageData.query.title}</h3>
 
-      <h3 className="Card-title">
-        {pageData.query.title}
-      </h3>
-
-      <Link
-        passHref
-        href={route}
-        as={pageData.query.path}
-        prefetch
-        scroll={false}
-      >
-
-        <a
-          className="Card-link"
-          title={pageData.query.title}
-        >
-          <span>
-            Go to: {pageData.query.title}
-          </span>
-        </a>
-      </Link>
-
-    </Card_Styled>
-  );
+			<Link
+				passHref
+				href={route}
+				as={pageData.query.path}
+				prefetch
+				scroll={false}
+			>
+				<a className="Card-link" title={pageData.query.title}>
+					<span>Go to: {pageData.query.title}</span>
+				</a>
+			</Link>
+		</Card_Styled>
+	);
 }
 
 export default Card;
 
 /**
  * =Cards
-************************************************************/
+ ************************************************************/
 
-export function cards (rels) {
-  let cards = [];
+export function cards(rels) {
+	let cards = [];
 
-  if (rels) {
-    console.log(rels);
+	rels.map((rel, i) => {
+		const relPageData = exportMap[rel];
 
-    rels.map((rel, i) => {
-        const relPageData = exportMap[rel];
-
-        if (relPageData && relPageData !== undefined) {
-          cards.push(
-            <Card
-              key={`card-${i}`}
-              pageData={relPageData}
-            />
-          );
-        }
-      });
-  }
-
-  return cards;
+		if (relPageData && relPageData !== undefined) {
+			cards.push(<Card key={`card-${i}`} pageData={relPageData} />);
+		}
+	});
+	return cards;
 }
