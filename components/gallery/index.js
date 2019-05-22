d284c8d927701d4ee34500f7b5fb8c70faea0f11
Luke Boga Mitchell <lbogamitchell@gmail.com>
2019-05-22 17:40:53 +0100

    Fix gallery scroll lock - DONE

--- components/gallery/index.js
+++ components/gallery/index.js
@@ -1,10 +1,11 @@
 /**
  * =Gallery
-************************************************************/
+ ************************************************************/
 
-import react, {Component} from "react";
+import react, { Component } from "react";
 import Lightbox from "react-image-lightbox";
 import styled from "styled-components";
+import { disableBodyScroll, enableBodyScroll } from "body-scroll-lock";
 
 // External parts
 import Button from "../button";
@@ -14,81 +15,109 @@ import GalleryStyle from "./gallery.style";
 
 // Mixins/Themes etc
 import { PUBLIC } from "../../lib/_config";
-import {_transition} from "../../assets/styles/mixins/_style"
+import { _transition } from "../../assets/styles/mixins/_style";
 
 /**
  * =Styles
-******************************/
+ ******************************/
 
 const FeaturedImage_Styled = styled.img`
-  ${_transition()};
+	${_transition()};
 
-  cursor: pointer;
-  opacity: 1;
+	cursor: pointer;
+	opacity: 1;
 
-  &:hover {
-    opacity: .75;
-  }
+	&:hover {
+		opacity: 0.75;
+	}
 `;
 
 /**
  * =Component
-******************************/
+ ******************************/
 
 export default class Gallery extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      photoIndex: 0,
-      isOpen: false,
-    };
-  }
+		this.state = {
+			photoIndex: 0,
+			isOpen: false
+		};
+	}
 
-  handleClick() {
-    this.setState({ isOpen: true })
-  }
+	targetElement = null;
+	componentDidMount() {
+		this.targetElement = document.querySelector("#Gallery");
+	}
 
-  render() {
-    const { photoIndex, isOpen } = this.state;
+	handleClick() {
+		if (!this.state.isOpen) {
+			disableBodyScroll(this.targetElement, {reserveScrollBarGap: true});
+		} else {
+			enableBodyScroll(this.targetElement);
+		}
+		this.setState({
+			isOpen: !this.state.isOpen ? true : false
+		});
+	}
 
-    return (
-      <div>
-		{/* <h2>Image gallery</h2> */}
+	render() {
+		const { photoIndex, isOpen } = this.state;
 
-		<Button
-          handleClick={this.handleClick.bind(this)}
-          options={{
-            type: "cta",
-            text: "View gallery"
-          }}
-        />
+		return (
+			<div>
+				<Button
+					handleClick={this.handleClick.bind(this)}
+					options={{
+						type: "cta",
+						text: "View gallery"
+					}}
+				/>
 
-        <FeaturedImage_Styled
-          onClick={this.handleClick.bind(this)}
-          src={`${PUBLIC.path}${this.props.images[0]}`} />
+				<FeaturedImage_Styled
+					onClick={this.handleClick.bind(this)}
+					src={`${PUBLIC.path}${this.props.images[0]}`}
+				/>
 
-        {isOpen && (
-          <Lightbox
-            mainSrc={`${PUBLIC.path}${this.props.images[photoIndex]}`}
-            nextSrc={`${PUBLIC.path}${this.props.images[(photoIndex + 1) % this.props.images.length]}`}
-            prevSrc={`${PUBLIC.path}${this.props.images[(photoIndex + this.props.images.length - 1) % this.props.images.length]}`}
-            onCloseRequest={() => this.setState({ isOpen: false })}
-            onMovePrevRequest={() =>
-              this.setState({
-                photoIndex: (photoIndex + this.props.images.length - 1) % this.props.images.length,
-              })
-            }
-            onMoveNextRequest={() =>
-              this.setState({
-                photoIndex: (photoIndex + 1) % this.props.images.length,
-              })
-            }
-          />
-        )}
+				{isOpen && (
+					<Lightbox
+						id={ "Gallery" }
+						mainSrc={`${PUBLIC.path}${
+							this.props.images[photoIndex]
+						}`}
+						nextSrc={`${PUBLIC.path}${
+							this.props.images[
+								(photoIndex + 1) % this.props.images.length
+							]
+						}`}
+						prevSrc={`${PUBLIC.path}${
+							this.props.images[
+								(photoIndex + this.props.images.length - 1) %
+									this.props.images.length
+							]
+						}`}
+						onCloseRequest={this.handleClick.bind(this)}
+						onMovePrevRequest={() =>
+							this.setState({
+								photoIndex:
+									(photoIndex +
+										this.props.images.length -
+										1) %
+									this.props.images.length
+							})
+						}
+						onMoveNextRequest={() =>
+							this.setState({
+								photoIndex:
+									(photoIndex + 1) % this.props.images.length
+							})
+						}
+					/>
+				)}
 
-        <GalleryStyle />
-      </div>
-    );
-  }
+				<GalleryStyle />
+			</div>
+		);
+	}
 }
